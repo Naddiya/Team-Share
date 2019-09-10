52d4a88649671c6aeda9685cdb88492613377c8d
Jlescieux <julien.lescieux@outlook.com>
2019-09-10 14:45:15 +0200

    add project ok

--- team-share-back/src/Controller/ProjectController.php
+++ team-share-back/src/Controller/ProjectController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Project;
+use App\Repository\TagRepository;
 use App\Repository\UserRepository;
+use App\Repository\SkillRepository;
 use App\Repository\StatutRepository;
+use App\Repository\TechnoRepository;
 use App\Repository\ProjectRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -48,25 +51,48 @@ class ProjectController extends AbstractController
     /**
      * @Route("/project/new", name="project_new", methods={"POST"})
      */
-    public function new(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, StatutRepository $statutRepository, UserRepository $userRepository)
+    public function new(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, TagRepository $tagRepository, StatutRepository $statutRepository, UserRepository $userRepository, SkillRepository $skillRepository, TechnoRepository $technoRepository)
     {
         // Récupére le contenu du json reçu
         $jsonContent = $request->getContent();
-        //dd($jsonContent->request->get('tags'));
-        //dd($jsonContent);
+
         // Déserialize le json et crée un objet Project avec les propriétés du json reçu
         $newProjectObject = $serializer->deserialize($jsonContent, Project::class, 'json');
-        //dd($newProjectObject->getTags());
+
+        foreach ($newProjectObject->getTags() as $tag) {
+          $tagBdd = $tagRepository->findOneBy(['name' => $tag->getName()]);
+          $newProjectObject->addTag($tagBdd);
+          $newProjectObject->removeTag($tag);
+        }
+
+        foreach ($newProjectObject->getUsers() as $user) {
+          $userBdd = $userRepository->findOneBy(['username' => $user->getUsername()]);
+          $newProjectObject->addUser($userBdd);
+          $newProjectObject->removeUser($user);
+        }
+
+        foreach ($newProjectObject->getTechnos() as $techno) {
+          $technoBdd = $technoRepository->findOneBy(['name' => $techno->getName()]);
+          $newProjectObject->addTechno($technoBdd);
+          $newProjectObject->removeTechno($techno);
+        }
+
+        foreach ($newProjectObject->getSkills() as $skill) {
+          $skillBdd = $skillRepository->findOneBy(['name' => $skill->getName()]);
+          $newProjectObject->addSkill($skillBdd);
+          $newProjectObject->removeSkill($skill);
+        }
+        
 
         // Récupère l'objet Statut "Not Start" et l'attribut par défaut
         $statut = $statutRepository->findOneBy(['name' => 'Not Start']);
         $newProjectObject->setStatut($statut);
 
         // User : pour le moment, allons chercher un user issue de notre liste
-        $user = $userRepository->findOneBy(['username' => $newProjectObject->getUsers()->getUsername()]);
-        // On associe<
-        dd($user);
-        $newProjectObject->addUser($user);
+        //$user = $userRepository->findOneBy(['username' => $newProjectObject->getUsers()->getUsername()]);
+        // On associe
+        //dd($user);
+        //$newProjectObject->addUser($user);
 
         // Enregistre le nouvel utilisateur en bdd
         $entityManager->persist($newProjectObject);
