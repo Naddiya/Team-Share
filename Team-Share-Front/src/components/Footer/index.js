268396732489576ee37d8cc32b0497f4647d0345
Nadia <nadiamedkouri@gmail.com>
2019-09-10 10:45:18 +0200

    responsive footer done

--- Team-Share-Front/src/components/Footer/index.js
+++ Team-Share-Front/src/components/Footer/index.js
@@ -35,7 +35,7 @@ const Footer = () => (
     </div>
     <div className="footer-bottom">
       <div className="footer-bottom-copyright">
-      &copy; 2019 Team-Share-Project.com Apothéose O'Clock
+      &copy; 2019 Team-Share-Project.io Apothéose O'Clock
       </div>
     </div>
     
