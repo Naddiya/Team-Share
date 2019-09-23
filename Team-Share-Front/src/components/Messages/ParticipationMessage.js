014702dd5fe998fde43500c46288fea69f105bd9
Alexisbej <a.bejrach@gmail.com>
2019-09-23 22:36:25 +0200

    route /follow/add on projectlist | error but seems to be good

--- Team-Share-Front/src/components/Messages/ParticipationMessage.js
+++ Team-Share-Front/src/components/Messages/ParticipationMessage.js
@@ -2,12 +2,6 @@ import React from 'react'
 import { Message, Button } from 'semantic-ui-react'
 import axios from 'axios';
 
-const list = [
-  'Josephine Backer',
-  'Charles Aznavour',
-]
-
-
 class ParticipationMessage extends React.Component {
 
   state = {
@@ -20,26 +14,27 @@ class ParticipationMessage extends React.Component {
       .then((response) => {
         const requests = response.data;
         console.log(requests);
+        console.log(this.state.token);
         this.setState({
           requests: requests,
-          response: requests.response,
         })
       })
       .catch((error) => {
           console.log(error);
+          console.log('token : ' + this.state.token);
       });
   }
 
   render() {  
-    return (
-          this.state.requests.map((request) => (
-            <Message info key={request.title}>{request.user.firstname} {request.user.lastname} - {request.title} 
-              <Button size="mini" color="green">Valider</Button>
-              <Button size="mini" color="red">Refuser</Button>
-            </Message>
-          ))
-      // <Message info header='Demandes de Participation' list={list} />
-    );
+    return null
+    //       // this.state.requests.map((request) => (
+    //       //   <Message info key={request.title}>{request.user.firstname} {request.user.lastname} - {request.title} 
+    //       //     <Button size="mini" color="green">Valider</Button>
+    //       //     <Button size="mini" color="red">Refuser</Button>
+    //       //   </Message>
+    //       // ))
+    //   // <Message info header='Demandes de Participation' list={list} />
+    // );
   };
 };
 export default ParticipationMessage;
--- Team-Share-Front/src/components/Projects/ProjectList.js
+++ Team-Share-Front/src/components/Projects/ProjectList.js
@@ -4,38 +4,60 @@ import { Button, Icon, Image, Item, Label } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 import { IoIosAddCircle, IoIosBatteryCharging } from "react-icons/io";
 import { NavLink } from 'react-router-dom'; 
+import axios from 'axios';
+import store from 'src/store';
 
 // == Import : local
 
 
 // == Composant
-const ProjectList = ({ image, title, tag, description, nbLike, id }) => (
-<Item>
-      <Item.Image src={image} />
+const ProjectList = ({ image, title, tag, description, nbLike, id }) => {
+  
+  const state = store.getState();
 
-      <Item.Content>
-        <Item.Header as={NavLink} to={`project-detail/${id}`}>{title}
-        </Item.Header>
-          <span className="item-follow"><IoIosAddCircle size="28px" />Follow</span>
-        <Item.Extra>
-          <Label>{tag}</Label>
-          <Label>le 20/12/2015</Label>
-          <a>
-            <Icon name='heart' />
-            {nbLike} likes
-        </a>
-        <span className="item-follow"><IoIosBatteryCharging size="28px" />Progression</span>
-        </Item.Extra>
-        <Item.Description>{description}</Item.Description>
-        <Item.Extra>
-          <span>Catégories : </span>
-          <Label>{tag}</Label>
-          <span>Compétences : </span>
-          <Label>{tag}</Label>
-        </Item.Extra>
-      </Item.Content>
-    </Item>
-);
+  const data = {
+    token: state.token,
+    id,
+  }
+  const handleClickLike = () => {
+    axios.post('http://92.243.10.99/Team-Share/team-share-back/public/follow/add', data)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log(data);
+    });
+  }
+
+  return (
+    <Item>
+        <Item.Image src={image} />
+
+        <Item.Content>
+          <Item.Header as={NavLink} to={`project-detail/${id}`}>{title}
+          </Item.Header>
+            <Button onClick={handleClickLike} className="item-follow"><IoIosAddCircle size="28px" />Follow</Button>
+          <Item.Extra>
+            <Label>{tag}</Label>
+            <Label>le 20/12/2015</Label>
+            <a>
+              <Icon name='heart' />
+              {nbLike} likes
+          </a>
+          <span className="item-follow"><IoIosBatteryCharging size="28px" />Progression</span>
+          </Item.Extra>
+          <Item.Description>{description}</Item.Description>
+          <Item.Extra>
+            <span>Catégories : </span>
+            <Label>{tag}</Label>
+            <span>Compétences : </span>
+            <Label>{tag}</Label>
+          </Item.Extra>
+        </Item.Content>
+      </Item>
+    );
+  };
 
 // == Export
 export default ProjectList;
