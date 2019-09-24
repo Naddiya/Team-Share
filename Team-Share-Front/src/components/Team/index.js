9c60f8b85ecbbdfaf28ee6fec0d0c37c3b2633da
Nadia <nadiamedkouri@gmail.com>
2019-09-24 15:00:49 +0200

    team pics

--- Team-Share-Front/src/components/Team/index.js
+++ Team-Share-Front/src/components/Team/index.js
@@ -11,24 +11,24 @@ const Team = () => (
         <h1 className="team-title">Notre fabuleuse équipe</h1>
         <div className="team-members">
             <div className="team-member">
-                <img className="team-image" src="http://92.243.10.99/pics/nadiapic.jpg" />
+                <img className="team-image" src="http://92.243.10.99/pics/nana.png" />
                 <h3 className="team-member-name">Nadia</h3>
                 <p className="team-member-role">Project Manager</p>
             </div>
             <div className="team-member">
-                <img className="team-image" src="http://92.243.10.99/pics/teamPic.png" />
+                <img className="team-image" src="http://92.243.10.99/pics/julien.jpg" />
                 <h3 className="team-member-name">Julien</h3>
                 <p className="team-member-role">Product Owner</p>
             </div>
             <div className="team-member">
-                <img className="team-image" src="http://92.243.10.99/pics/teamPic.png" />
+                <img className="team-image" src="http://92.243.10.99/pics/alexandre.png" />
                 <h3 className="team-member-name">Alexandre</h3>
-                <p className="team-member-role">Lead Dev Back</p>
+                <p className="team-member-role">Lead Back</p>
             </div>
             <div className="team-member">
-                <img className="team-image" src="http://92.243.10.99/pics/teamPic.png" />
+                <img className="team-image" src="http://92.243.10.99/pics/alexis.png" />
                 <h3 className="team-member-name">Alexis</h3>
-                <p className="team-member-role">Lead Dev Front</p>
+                <p className="team-member-role">Lead Front</p>
             </div>
         </div>
     </div>
